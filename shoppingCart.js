08e91d136b0110bab2f3c5d68d9230f8719ce3b3
DanielLorenzo22 <daniellorenzocabrera@gmail.com>
2022-01-12 12:45:56 +0000

    Creating items

--- shoppingCart.js
+++ shoppingCart.js
@@ -2,18 +2,30 @@ document.addEventListener("DOMContentLoaded", init)
 
 
 function init(){
+    const main = document.querySelector("main");
     getProducts().then(data => {
         console.log(data)
-        const first = data[0];
-        const main = document.querySelector("main");
-        const img = `<img src='resources/${first.img}'>`;
-        main.append(img)
+        data.forEach(product =>{
+            main.innerHTML += `
+            <article class='item'>
+            <img src='resources/${product.img}'>
+            <p>${product.name}</p>
+            <p>${product.cost} €</p>
+            <input type='number' min='0'>
+            </article>
+            `
+        })
 
+       
+        /*
+        const first = data[2];
+        const img = `<img src='resources/${first.img}'>`;
+        main.innerHTML = img;
+*/
 
     });
-    
-    
 
+    
 
 }
 
